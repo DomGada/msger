976be98777f07167f57fd5ec35f0f6d1cda879ae
= <=>
2021-07-21 15:10:32 -0700

    Added two get routes for Users, implemented login and name check for users.

--- msgServer/routes/users.js
+++ msgServer/routes/users.js
@@ -5,20 +5,41 @@ const Users = require("../models/Users");
 const testUser = require("../testUser.json");
 
 router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, HEAD, OPTIONS")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+router.get('/name', function (req, res, next) {
+    Users.findOne({
+        where: {
+            id: req.query.id
+        }
+    }).then((Users) => {
+        if (Users) {
+            res.json(Users.firstname);
+        } else {
+            res.json(false);
+        }
+    })
+})
 
-router.get('/', function (req, res, next) {
-    Users.findAll()
-        .then((Users) => {
-            console.log(Users);
-            res.sendStatus(200);
-        })
-        .catch((err) => console.log(err))
-});
+router.get('/login', function (req, res, next) {
+    Users.findOne({
+        where: {
+            username: req.query.username,
+            password: req.query.password,
+        }
+    }).then((Users) => {
+        if (Users) {
+            res.json(Users.id)
+        } else {
+            res.json(false)
+        }
+    })
+})
+
+
 
 router.post('/insert', function (req, res, next) {
     console.log("The body is :", req.body)
