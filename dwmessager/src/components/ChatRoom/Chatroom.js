44efcb5babb61d08bd51714a8156b3c854f09d16
= <=>
2021-07-22 10:59:38 -0700

    Added chatroom Component

---
+++ dwmessager/src/components/ChatRoom/Chatroom.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Nav from "./Navbar/Nav";
+const useStyles = makeStyles({});
+const Chatroom = () => {
+  const classes = useStyles();
+  return <div></div>;
+};
+
+export default Chatroom;
